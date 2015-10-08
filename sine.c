f972546c27bbeca5d7228c30947c6c6ffda9cd78
Kamilah Jenkins <kamilah.jenkins@gmail.com>
2015-10-08 16:43:05 -0700

    changes the frambuffer

---
+++ sine.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <math.h>
+
+/* Image parameters */
+#define W 320
+#define H 256
+#define M 255
+
+/* This is where we'll store the pixels */
+struct pixel { uint8_t r, g, b, a; };
+struct pixel fb[W][H];
+struct pixel PINK;
+PINK.r = 240;
+PINK.g = 54;
+PINK.b = 87;
+PINK.a = 0;
+
+/* Draw a full 2*pi period of the sine */
+void
+create_image ( void )
+{
+        /* Run the width of the image */
+        for ( int x=0; x<W; x++ )
+        {
+            int y = H/2;
+            fb[x][y] = PINK;
+        }
+}
+
+/* Write the ppm-formatted file */
+void
+write_ppm ( char *filename )
+{
+        FILE *out = fopen ( filename, "w" );
+            /* Header:
+             * Magic number, width, height, maxvalue
+             */
+            fprintf ( out, "P6 %d %d %d\n", W, H, M );
+                /* Rows. 2-byte values if max > 255, 1-byte otherwise.
+                 * These loops explicitly show where every single byte
+                 * goes; in practice, it would be faster and shorter to
+                 * interleave the arrays and write bigger blocks of
+                 * contiguous data.
+                 */
+                for ( size_t i=0; i<H; i++ )
+                            for ( size_t j=0; j<W; j++ )
+                            {
+                               fwrite ( &r[i][j], sizeof(uint8_t), 1, out ); /* Red */
+                               fwrite ( &g[i][j], sizeof(uint8_t), 1, out ); /* Green */
+                               fwrite ( &b[i][j], sizeof(uint8_t), 1, out ); /* Blue */
+                            }
+                fclose ( out );
+}
+
+/* Make the picture, store it, and go home */
+int
+main ()
+{
+        create_image();
+            write_ppm ( "sine.ppm" );
+                exit ( EXIT_SUCCESS );
+}
