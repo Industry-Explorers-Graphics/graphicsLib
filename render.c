7bc048952fd474d68b31529d3ffdb51eb4f2b45a
Anne Kimsey <anne.kimsey@gmail.com>
2015-10-16 10:57:22 -0700

    manual merge

--- render.c
+++ render.c
@@ -22,15 +22,16 @@ void setPixel ( FrameBuffer *fb, int x, int y, Pixel color )
     fb->data[offset] = color;
 }
 
-/* Return color when pixel location is given */
-// I'm still working on this
-Pixel getPixel ( FrameBuffer *fb, int x, int y)
-{
-    //return Pixel color;      
+/* Does this work? */
+/* Will need to be changed once we get rid of the pixel struct anyway */
+Pixel getPixel(FrameBuffer *fb, int x, int y) {
+    int offset = y * fb-> width + x;
+    Pixel color = fb -> data[offset];
+    return color;
 }
 
 /* Create  a horizontal line on FrameBuffer */
-void horizontalLine ( FrameBuffer *fb, int length, int x, int y, Pixel color )
+void drawHorizontalLine ( FrameBuffer *fb, int length, int x, int y, Pixel color )
 {
     for( int i = x; i < ( x + length ); i++ )
     {
@@ -39,7 +40,7 @@ void horizontalLine ( FrameBuffer *fb, int length, int x, int y, Pixel color )
 }
 
 /* Draw a vertical line */
-void verticalLine ( FrameBuffer *fb, int length, int x, int y, Pixel color )
+void drawVerticalLine ( FrameBuffer *fb, int length, int x, int y, Pixel color )
 {
     for( int i = y; i < ( y + length ); i++ )
     {
@@ -65,7 +66,7 @@ int sign( int number )
     }
 }
 
-void diagonalLine ( FrameBuffer *fb, int x1, int y1, int x2, int y2, Pixel color ) 
+void drawDiagonalLine ( FrameBuffer *fb, int x1, int y1, int x2, int y2, Pixel color ) 
 {
     
     int i, dx, dy, sdx, sdy, dxabs, dyabs, x, y, xStart, yStart;
@@ -118,7 +119,8 @@ void diagonalLine ( FrameBuffer *fb, int x1, int y1, int x2, int y2, Pixel color
 
 
 /* Draw a rectangle with square corners*/
-void squaredRect ( FrameBuffer *fb, int length, int width, int x, int y, Pixel color )
+/* outline of rect function */
+void drawSquaredRect ( FrameBuffer *fb, int length, int width, int x, int y, Pixel color )
 {
    if ( length > 0 && width > 0 )
    {
@@ -139,39 +141,114 @@ void squaredRect ( FrameBuffer *fb, int length, int width, int x, int y, Pixel c
 
 /* Create a rectangle with rounded corners  */
 
-/* Create a triangle using the diagonal line function */
-// Order is important for this implementation!
-void triangle ( FrameBuffer *fb, int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2, int cx1,  int cy1, int cx2, int cy2, Pixel color )
+void drawFillRect(FrameBuffer * fb, int x, int y, int width, int height, Pixel color)
 {
-    diagonalLine( fb, ax1, ay1, ax2, ay2, color );
-    diagonalLine( fb, bx1, by1, bx2, by2, color );
-    diagonalLine( fb, ax1, cy1, cx2, cy2, color );
+    for (int i = x; i <= (x + width); i++)
+    {
+        drawVerticalLine(fb, i, y, height, color);
+    }
 }
 
-void triangle (   )
-{
-    drawTriangle()
-    {
-        /* at first sort the three vertices by y-coordinate ascending so v1 is the topmost vertice */
-          sortVerticesAscendingByY();
+/* Create a triangle using the diagonal line function */
+// Order is important for this implementation!
+//void drawTriangle ( FrameBuffer *fb, int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2, int cx1,  int cy1, int cx2, int cy2, Pixel color )
+//{
+//    drawDiagonalLine( fb, ax1, ay1, ax2, ay2, color );
+//    drawDiagonalLine( fb, bx1, by1, bx2, by2, color );
+//    drawDiagonalLine( fb, ax1, cy1, cx2, cy2, color );
+//}
+//
+//void triangle (   )
+//{
+//    drawTriangle()
+//    {
+//        /* at first sort the three vertices by y-coordinate ascending so v1 is the topmost vertice */
+//          sortVerticesAscendingByY();
+//
+//        /* here we know that v1.y <= v2.y <= v3.y */
+//        /* check for trivial case of bottom-flat triangle */
+//        if (v2.y == v3.y)
+//                  {
+//                          fillBottomFlatTriangle(v1, v2, v3);
+//                            }
+//        /* check for trivial case of top-flat triangle */
+//        else if (vt1.y == vt2.y)
+//                    {
+//                            fillTopFlatTriangle(g, vt1, vt2, vt3);
+//                              }
+//        else
+//        {
+//                              /* general case - split the triangle in a topflat and bottom-flat one */
+//                              Vertice v4 = new Vertice(
+//                                            (int)(vt1.x + ((float)(vt2.y - vt1.y) / (float)(vt3.y - vt1.y)) * (vt3.x - vt1.x)), vt2.y);
+//                                  fillBottomFlatTriangle(g, vt1, vt2, v4);
+//                                      fillTopFlatTriangle(g, vt2, v4, vt3);
+//                                        }
+//    }
 
-        /* here we know that v1.y <= v2.y <= v3.y */
-        /* check for trivial case of bottom-flat triangle */
-        if (v2.y == v3.y)
-                  {
-                          fillBottomFlatTriangle(v1, v2, v3);
-                            }
-        /* check for trivial case of top-flat triangle */
-        else if (vt1.y == vt2.y)
-                    {
-                            fillTopFlatTriangle(g, vt1, vt2, vt3);
-                              } 
-        else
-        {
-                              /* general case - split the triangle in a topflat and bottom-flat one */
-                              Vertice v4 = new Vertice( 
-                                            (int)(vt1.x + ((float)(vt2.y - vt1.y) / (float)(vt3.y - vt1.y)) * (vt3.x - vt1.x)), vt2.y);
-                                  fillBottomFlatTriangle(g, vt1, vt2, v4);
-                                      fillTopFlatTriangle(g, vt2, v4, vt3);
-                                        }
+void drawCircle(FrameBuffer *fb, int x0, int y0, int radius, Pixel color)
+{
+  int x = radius;
+  int y = 0;
+  int decisionOver2 = 1 - x;
+
+  while( y <= x )
+  {
+    setPixel(fb,  x + x0,  y + y0, color); // Octant 1
+    setPixel(fb,  y + x0,  x + y0, color); // Octant 2
+    setPixel(fb, -x + x0,  y + y0, color); // Octant 4
+    setPixel(fb, -y + x0,  x + y0, color); // Octant 3
+    setPixel(fb, -x + x0, -y + y0, color); // Octant 5
+    setPixel(fb, -y + x0, -x + y0, color); // Octant 6
+    setPixel(fb,  x + x0, -y + y0, color); // Octant 8
+    setPixel(fb,  y + x0, -x + y0, color); // Octant 7
+    y++;
+    if (decisionOver2<=0)
+    {
+      decisionOver2 += 2 * y + 1;   // Change in decision criterion for y -> y+1
     }
+    else
+    {
+      x--;
+      decisionOver2 += 2 * (y - x) + 1;   // Change for y -> y+1, x -> x-1
+    }
+  }
+}
+
+void drawEllipse (FrameBuffer * fb, int xc, int yc, int width, int height, Pixel color)
+{
+    int a2 = width * width;
+    int b2 = height * height;
+    int fa2 = 4 * a2, fb2 = 4 * b2;
+    int x, y, sigma;
+
+    /* first half */
+    for (x = 0, y = height, sigma = 2*b2+a2*(1-2*height); b2*x <= a2*y; x++)
+    {
+        setPixel(fb, xc + x, yc + y, color);
+        setPixel(fb, xc - x, yc + y, color);
+        setPixel(fb, xc + x, yc - y, color);
+        setPixel(fb, xc - x, yc - y, color);
+        if (sigma >= 0)
+        {
+            sigma += fa2 * (1 - y);
+            y--;
+        }
+        sigma += b2 * ((4 * x) + 6);
+    }
+
+    /* second half */
+    for (x = width, y = 0, sigma = 2*a2+b2*(1-2*width); a2*y <= b2*x; y++)
+    {
+        setPixel(fb, xc + x, yc + y, color);
+        setPixel(fb, xc - x, yc + y, color);
+        setPixel(fb, xc + x, yc - y, color);
+        setPixel(fb, xc - x, yc - y, color);
+        if (sigma >= 0)
+        {
+            sigma += fb2 * (1 - x);
+            x--;
+        }
+        sigma += a2 * ((4 * y) + 6);
+    }
+}
--- render.h
+++ render.h
@@ -3,6 +3,9 @@
 #include <stdint.h>
 #include <math.h>
 
+#ifndef render_h
+#define render_h
+
 /* Image parameters */
 /* Create the pixel bytes */
 typedef struct pixel
@@ -19,8 +22,19 @@ typedef struct frameBuffer
 
 static const Pixel PINK = { 240, 54, 87 };
 FrameBuffer *createFrameBuffer ( int width, int height );
-void horizontalLine ( FrameBuffer *fb, int length, int x, int y, Pixel color );
-void verticalLine ( FrameBuffer *fb, int length, int x,  int y, Pixel color );
-void squaredRect ( FrameBuffer *fb, int length, int width, int x, int y, Pixel color );
-void diagonalLine ( FrameBuffer *fb, int x1, int y1, int x2, int y2, Pixel color );
-void triangle ( FrameBuffer *fb, int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2, int cx1, int cy1, int cx2, int cy2, Pixel color );
+//Pixel getPixel(FrameBuffer * fb, int x, int y);
+
+void drawHorizontalLine ( FrameBuffer *fb, int length, int x, int y, Pixel color );
+void drawVerticalLine ( FrameBuffer *fb, int length, int x,  int y, Pixel color );
+void drawDiagonalLine ( FrameBuffer *fb, int x1, int y1, int x2, int y2, Pixel color );
+
+void drawSquaredRect ( FrameBuffer *fb, int length, int width, int x, int y, Pixel color );
+void drawFillRect(FrameBuffer * fb, int x, int y, int width, int height, Pixel color);
+
+//void triangle ( FrameBuffer *fb, int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2, int cx1, int cy1, int cx2, int cy2, Pixel color );
+//void drawTriangle ( FrameBuffer *fb, int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2, int cx1,  int cy1, int cx2, int cy2, Pixel color );
+
+void drawCircle(FrameBuffer *fb, int x0, int y0, int radius, Pixel color);
+void drawEllipse (FrameBuffer * fb, int xc, int yc, int width, int height, Pixel color);
+
+#endif
