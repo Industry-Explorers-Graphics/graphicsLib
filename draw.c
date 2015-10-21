fa6e8c634ddfaca869aed8eec3315a58a0413fe3
Anne Kimsey <anne.kimsey@gmail.com>
2015-10-21 15:38:41 -0700

    add bitblt example to draw file

--- draw.c
+++ draw.c
@@ -17,7 +17,8 @@ void write_ppm(char *filename, FrameBuffer * fb)
 int main ()
 {
     FrameBuffer *fb =  createFrameBuffer( 320, 256 );
-//    drawRect(fb, 1, 1,  100, 200, PINK);
+    FrameBuffer *dst = createFrameBuffer(400, 340);
+//    drawFillRect(fb, 1, 1,  100, 200, PINK);
 //    drawCircle(fb, 100, 100, 10, PINK);
 //    drawEllipse(fb, 100, 100, 10, 20, PINK);
 //    triangle ( fb, 30, 50, 50, 20, 50, 20, 10, 20, 30, 50, 10, 20, PINK );
@@ -27,6 +28,7 @@ int main ()
 //    bezier(fb, 350, 100, 550, 10, 600, 100, PINK);
 //    write_ppm ( "bezier.ppm", fb );
     drawFillTriangle ( fb, 30, 50, 10, 20, 50, 40, PINK );
-    write_ppm ( "fillTriangle.ppm", fb );
+    bitBlt(dst, fb, 300, 200);
+    write_ppm ( "bitBltTest.ppm", dst );
     return 0;
 }
