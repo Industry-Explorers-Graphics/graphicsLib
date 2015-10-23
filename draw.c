ac8c21fca6122b4acf446b65f15e51b08e9a3798
Anne Kimsey <anne.kimsey@gmail.com>
2015-10-23 14:52:05 -0700

    resolve merge conflict

--- draw.c
+++ draw.c
@@ -18,30 +18,30 @@ int main ()
 {
     frameBuffer *fb =  createFrameBuffer( 320, 256, 0, 0, 0, 1, 0 );
     frameBuffer *dst = createFrameBuffer( 400, 340, 0, 0, fb->data, 0, fb->pixelStride );
-
-    frameBuffer *fb2 =  createFrameBuffer( 320, 256, 0, 0, 0, 1, 0 );
-    drawRectFrame( fb2, 1, 1,  100, 200, PINK );
-    write_ppm( "rectFrame.ppm", fb2 );
-
-    frameBuffer *fb3 =  createFrameBuffer( 320, 256, 0, 0, 0, 1, 0 );
-    drawCircleFrame( fb3, 100, 100, 10, PINK );
-    write_ppm( "circleFrame.ppm", fb3 );
-
-    frameBuffer *fb4 =  createFrameBuffer( 320, 256, 0, 0, 0, 1, 0 );
-    drawEllipseFrame( fb4, 100, 100, 10, 20, PINK );
-    write_ppm( "ellipseFrame.ppm", fb4 );
-
-    frameBuffer *fb5 =  createFrameBuffer( 320, 256, 0, 0, 0, 1, 0 );
-    drawTriangleFrame( fb5, 30, 50, 50, 20, 50, 20, 10, 20, 30, 50, 10, 20, PINK );
-    write_ppm( "triFrame.ppm", fb5 );
-
-    frameBuffer *fb6 =  createFrameBuffer( 320, 256, 0, 0, 0, 1, 0 );
-    bezier(fb6, 350, 100, 550, 10, 600, 100, PINK);
-    write_ppm( "bezier.ppm", fb6 );
-
-    frameBuffer *fb7 =  createFrameBuffer( 320, 256, 0, 0, 0, 1, 0 );
-    drawTriangleFill( fb7, 30, 50, 10, 20, 50, 40, PINK );
-    write_ppm( "trianleFrame.ppm", fb7 );
+//
+//    frameBuffer *fb2 =  createFrameBuffer( 320, 256, 0, 0, 0, 1, 0 );
+//    drawRectFrame( fb2, 1, 1,  100, 200, PINK );
+//    write_ppm( "rectFrame.ppm", fb2 );
+//
+//    frameBuffer *fb3 =  createFrameBuffer( 320, 256, 0, 0, 0, 1, 0 );
+//    drawCircleFrame( fb3, 100, 100, 10, PINK );
+//    write_ppm( "circleFrame.ppm", fb3 );
+//
+//    frameBuffer *fb4 =  createFrameBuffer( 320, 256, 0, 0, 0, 1, 0 );
+//    drawEllipseFrame( fb4, 100, 100, 10, 20, PINK );
+//    write_ppm( "ellipseFrame.ppm", fb4 );
+//
+//    frameBuffer *fb5 =  createFrameBuffer( 320, 256, 0, 0, 0, 1, 0 );
+//    drawTriangleFrame( fb5, 30, 50, 50, 20, 50, 20, 10, 20, 30, 50, 10, 20, PINK );
+//    write_ppm( "triFrame.ppm", fb5 );
+//
+//    frameBuffer *fb6 =  createFrameBuffer( 320, 256, 0, 0, 0, 1, 0 );
+//    bezier(fb6, 350, 100, 550, 10, 600, 100, PINK);
+//    write_ppm( "bezier.ppm", fb6 );
+//
+//    frameBuffer *fb7 =  createFrameBuffer( 320, 256, 0, 0, 0, 1, 0 );
+    drawTriangleFill( fb, 30, 50, 10, 20, 50, 40, PINK );
+//    write_ppm( "triangleFrame.ppm", fb );
 
     bitBlt(dst, 3, 2, fb, 10, 20, 60, 60 );
     write_ppm ( "bitBltTest.ppm", dst );
