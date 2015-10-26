a3f2ddd680c17cf3bbdfabd81d2d20cdcb65df06
KamilahJ4e <kamilahjae@gmail.com>
2015-10-25 22:41:55 -0700

    starts triangulate functionality

--- render.c
+++ render.c
@@ -540,3 +540,189 @@ void drawText(frameBuffer *fb, int px, int py, char* text, pixel color) {
             px+=8;
     }
 }
+
+/* Triangulate a polygon and fill */
+int pointInPolygon( int polyCorners, int polyX[], int polyY[], int x, int y )
+{
+	int   i, j = polyCorners - 1;
+	int  oddNodes = 0;
+
+	for ( i = 0; i < polyCorners; i++ )
+	{
+		if ( ( polyY[ i ]< y && polyY[ j ] >= y
+			|| polyY[ j ]< y && polyY[ i ] >= y )
+			&& ( polyX[ i ] <= x || polyX[ j ] <= x ) )
+		{
+			oddNodes ^= ( polyX[ i ] + ( y - polyY[ i ] ) / ( polyY[ j ] - polyY[ i ] )*( polyX[ j ] - polyX[ i ] ) < x );
+		}
+		j = i;
+	}
+
+	return oddNodes;
+}
+
+float triangleArea( point *contour )
+{
+	int n = contour.size( );
+
+	float A = 0.0f;
+
+	for ( int p = n - 1, q = 0; q < n; p = q++ )
+	{
+		A += contour[ p ]._x * contour[ q ]._y - contour[ q ]._x * contour[ p ]._y;
+	}
+	return A*0.5f;
+}
+
+/*
+triangulate_inside_triangle
+decides if a point P is Inside of the triangle
+defined by A, B, C.
+*/
+bool triangle_pt_inside( float Ax, float Ay,
+	float Bx, float By,
+	float Cx, float Cy,
+	float Px, float Py )
+
+{
+	float ax, ay, bx, by, cx, cy, apx, apy, bpx, bpy, cpx, cpy;
+	float cCROSSap, bCROSScp, aCROSSbp;
+
+	ax = Cx - Bx;  ay = Cy - By;
+	bx = Ax - Cx;  by = Ay - Cy;
+	cx = Bx - Ax;  cy = By - Ay;
+	apx = Px - Ax;  apy = Py - Ay;
+	bpx = Px - Bx;  bpy = Py - By;
+	cpx = Px - Cx;  cpy = Py - Cy;
+
+	aCROSSbp = ax*bpy - ay*bpx;
+	cCROSSap = cx*apy - cy*apx;
+	bCROSScp = bx*cpy - by*cpx;
+
+	return ( ( aCROSSbp >= 0.0f ) && ( bCROSScp >= 0.0f ) && ( cCROSSap >= 0.0f ) );
+};
+
+bool triangulateSnip( point *contour, int u, int v, int w, int n, int *V )
+{
+	int p;
+	float Ax, Ay, Bx, By, Cx, Cy, Px, Py;
+
+	Ax = contour[ V[ u ] ]._x;
+	Ay = contour[ V[ u ] ]._y;
+
+	Bx = contour[ V[ v ] ]._x;
+	By = contour[ V[ v ] ]._y;
+
+	Cx = contour[ V[ w ] ]._x;
+	Cy = contour[ V[ w ] ]._y;
+
+	if ( EPSILON > ( ( ( Bx - Ax )*( Cy - Ay ) ) - ( ( By - Ay )*( Cx - Ax ) ) ) ) return false;
+
+	for ( p = 0; p<n; p++ )
+	{
+		if ( ( p == u ) || ( p == v ) || ( p == w ) ) continue;
+		Px = contour[ V[ p ] ]._x;
+		Py = contour[ V[ p ] ]._y;
+		if ( triangle_pt_inside( Ax, Ay, Bx, By, Cx, Cy, Px, Py ) ) return false;
+	}
+
+	return true;
+}
+
+bool triangulate_process( point *contour )
+{
+	// allocate and initialize list of Vertices in polygon
+	int n = contour.size(  );
+	if ( n < 3 ) return false;
+
+	int *V = int[ n ];
+
+	// we want a counter-clockwise polygon in V
+
+	if ( 0.0f < triangleArea( contour ) )
+	for ( int v = 0; v < n; v++ )
+		V[ v ] = v;
+	else
+		for ( int v = 0; v < n; v++ )
+			V[ v ] = ( n - 1 ) - v;
+
+	int nv = n;
+
+	/*  remove nv-2 Vertices, creating 1 triangle every time */
+	int count = 2 * nv;   /* error detection */
+
+	for ( int m = 0, v = nv - 1; nv>2; )
+	{
+		/* if we loop, it is probably a non-simple polygon */
+		if ( 0 >= ( count-- ) )
+		{
+			//** Triangulate: ERROR - probable bad polygon!
+			return false;
+		}
+
+		/* three consecutive vertices in current polygon, <u,v,w> */
+		int u = v; if ( nv <= u ) u = 0;     /* previous */
+		v = u + 1; if ( nv <= v ) v = 0;     /* new v    */
+		int w = v + 1; if ( nv <= w ) w = 0;     /* next     */
+
+		if ( triangulateSnip( contour, u, v, w, nv, V ) )
+		{
+			int a, b, c, s, t;
+
+			/* true names of the vertices */
+			a = V[ u ]; b = V[ v ]; c = V[ w ];
+
+			// draw single triangle
+			triangle( contour[ a ]._x, contour[ a ]._y,
+				contour[ b ]._x, contour[ b ]._y,
+				contour[ c ]._x, contour[ c ]._y );
+
+			m++;
+
+			// remove v from remaining polygon
+			for ( s = v, t = v + 1; t<nv; s++, t++ )
+				V[ s ] = V[ t ];
+			nv--;
+
+			// resest error detection counter
+			count = 2 * nv;
+		}
+	}
+
+	delete V;
+
+	return true;
+}
+
+void polygon( const point *pts )
+{
+	COLOR oldStroke = strokeColor;
+
+	//  Invoke the triangulator to render the polygon as triangles
+	noStroke(  );
+	bool processed = triangulate_process( pts );
+
+	stroke( oldStroke );
+	if ( processed ) {
+		lineloop( pts );
+	}
+}
+
+void polygon( int nverts, int *verts )
+{
+	point a;
+	for ( int idx = 0; idx < nverts; idx++ ){
+		a.push_back( Vector2d( verts[ idx*2+0 ], verts[ idx*2+1 ] ) );
+	}
+
+
+	//  Invoke the triangulator to render the polygon as triangles
+	noStroke(  );
+	bool processed = triangulate_process( a );
+
+	stroke( strokeColor );
+	if ( processed ) {
+		// draw the outline
+		lineloop( a );
+	}
+}
