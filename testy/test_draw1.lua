9ee7dbd4011c21c4de47ff57b625137c34c20550
ASKimZ <anne.kimsey@gmail.com>
2015-10-28 16:19:11 -0700

    final wrapper

--- testy/test_draw1.lua
+++ testy/test_draw1.lua
@@ -8,12 +8,12 @@ local ppm = require("ppm")
 
 -- Create a framebuffer
 local width = 320;
-local height = 240;
+local height = 256;
 local x = 0;
 local y = 0;
 local data = nil;
-local ownsData = true;
-local pixelStride = 320;
+local ownsData = 1;
+local pixelStride = 0;
 
 local fb = draw.createFrameBuffer ( width, height, x, y, data, ownsData, pixelStride );
 assert(fb ~= nil)
@@ -24,15 +24,15 @@ local color = ffi.new("pixel", 255, 0, 0);
 draw.drawHorizontalLine( fb, width-1, 0, 10, color );
 draw.drawHorizontalLine( fb, width-1, 0, height-10, color );
 
---draw.drawVerticalLine( fb, height-1, 10,  10, color );
---draw.drawVerticalLine( fb, int length, width-1,  height-1, color );
+draw.drawVerticalLine( fb, height-1, 10,  0, color );
+draw.drawVerticalLine( fb, height-1, width-1, 0, color );
 
 local color = ffi.new("pixel", 0,255,0);
 draw.drawDiagonalLine( fb, 0, 0, width-1, height-1, color );
 draw.drawDiagonalLine( fb, width-1, 0, 0, height-1, color );
 
 -- write it out to a ppm file
-ppm.write_PPM_binary("test_draw1.ppm", fb.data, fb.width, fb.height, fb.pixelStride*ffi.sizeof("pixel"))
+ppm.write_PPM_binary("is_a_test.ppm", fb.data, fb.width, fb.height, 3*width)
 
 
 -- The End
