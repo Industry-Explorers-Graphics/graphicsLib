6393f2c728ea7c8406b65af1056a4685369cb7e1
ASKimZ <anne.kimsey@gmail.com>
2015-11-02 17:59:29 -0800

    start of refactors, draws a label

--- testy/scene.lua
+++ testy/scene.lua
@@ -3,12 +3,13 @@ local ffi = require("ffi")
 local draw = require("render_ffi")
 local colors = require("colors")
 local ppm = require("ppm")
+local lbl = require("label")
 
 -- Create framebuffer
--- TODO: extract this to it's own file
+-- TODO: extract this to its own file
 
-local width = 320;
-local height = 256;
+local width = 340;
+local height = 100;
 local x = 0;
 local y = 0;
 local data = nil;
@@ -21,27 +22,18 @@ assert(fb ~= nil)
 function BoundingBox()
   Box = {
   Length = 100,
-  Width = 110, 
-  X = 100,
-  Y = 100,
-  Color = colors.PINK,
-  Children = {},
+  Width = 100, 
+  X = fb.x + 5,
+  Y = fb.y + 5,
+  Color = colors.GREEN,
+  Children = {
+    {["type"] = "text", ["attr"] = "Hello World!"}},
+    {["type"] = "text", ["attr"] = "...               Goodbye world." }
   }
+
   return Box
-  --self.Length = params.Length or 1;
-  --self.Width = params.Width or 1;
-  --self.X = params.X or 100;
-  --self.Y = params.Y or 200;
-  --self.Color = params.color or PINK;
-  --self.Children = params.Children or {};
-  
-  --self:super(params)
-    
 end
 
-local bbox = BoundingBox()
-draw.drawRectFrame(fb, bbox.Length, bbox.Width, bbox.X, bbox.Y, bbox.Color);
---for _,child in ipairs(children) do
---  draw.Text(child.attributes)
-print(ffi)
-ppm.write_PPM_binary("bbox.ppm", fb.data, fb.width, fb.height, 3*width)
+myLabel = lbl:new(5,5,"hello",colors.PINK)
+myLabel:draw(fb)
+ppm.write_PPM_binary("label.ppm", fb.data, fb.width, fb.height, 3*width)
