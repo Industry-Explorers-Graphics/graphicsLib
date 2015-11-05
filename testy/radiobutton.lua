3c321b58378d9dda8f45d223eb33948abb2cd078
ASKimZ <anne.kimsey@gmail.com>
2015-11-05 15:40:06 -0800

    add radio button

--- testy/radiobutton.lua
+++ testy/radiobutton.lua
@@ -6,4 +6,23 @@ local RadioButton_mt = {
   __index = RadioButton;
 }
 
-function RadioButton.new(self, 
+function RadioButton.new(self, x, y, color, state)
+  local obj = {
+    x = x;
+    y = y;
+    radius = 5;
+    color = color;
+    state = state;
+  }
+  setmetatable(obj, RadioButton_mt)
+  return obj
+end 
+
+function RadioButton.draw(self, fb)
+  if self.state == 0 
+    drawLib.drawCircleFill(fb, self.x, self.y, self.radius, self.color)
+    drawLibdrawCirlceFill(fb, self.x, self.y, self.radius - 2, self.color)
+  end
+end
+
+return RadioButton
