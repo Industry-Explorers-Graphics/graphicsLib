1aba3907ded355a4df39c5a75d9a91f9668ae70b
ASKimZ <anne.kimsey@gmail.com>
2015-11-05 16:28:08 -0800

    add radio button:

--- testy/radiobutton.lua
+++ testy/radiobutton.lua
@@ -1,5 +1,6 @@
 --radiobutton.lua
 local drawLib = require("render_ffi")
+local colors = require("colors")
 
 local RadioButton = {}
 local RadioButton_mt = {
@@ -19,9 +20,14 @@ function RadioButton.new(self, x, y, color, state)
 end 
 
 function RadioButton.draw(self, fb)
-  if self.state == 0 
+  if self.state == 0 then
     drawLib.drawCircleFill(fb, self.x, self.y, self.radius, self.color)
-    drawLibdrawCirlceFill(fb, self.x, self.y, self.radius - 2, self.color)
+    drawLib.drawCircleFill(fb, self.x, self.y, self.radius - 2, colors.BLACK)
+  end
+  if self.state == 1 then
+    drawLib.drawCircleFill(fb, self.x, self.y, self.radius, self.color)
+    drawLib.drawCircleFill(fb, self.x, self.y, self.radius - 2, colors.BLACK)
+    drawLib.drawCircleFill(fb, self.x, self.y, self.radius - 3, self.color);
   end
 end
 
